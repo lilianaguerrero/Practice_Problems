25bc0a6456b3ecc5495ba182855b1fabe0058385
LilianaGuerrero <liliana.guerrerosf@gmail.com>
2020-09-09 15:46:30 -0700

    added ll method

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,26 @@
 #linked list
 
 class Element(object):
-    def __init__(self, value):
+    def __init__(self,value):
         self.value = value
         self.next = None
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        if self.head:
+            while current.next:
+                current = current.next
+                current.next = new_element
+        else:
+            self.head = new_element
+        
+
+
+
+
+
+
+
