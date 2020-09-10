35b7e74e3df100cc3ad263597c9fac8277b2b74a
LilianaGuerrero <liliana.guerrerosf@gmail.com>
2020-09-10 14:23:46 -0700

    add prefix trie

--- linked_list.py
+++ linked_list.py
@@ -6,16 +6,28 @@ class Element(object):
         self.next = None
 
 class LinkedList(object):
-    def __init__(self, head=None):
+    def __init__(self,head=None):
         self.head = head
 
-    def append(self, new_element):
+    def append(self,new_element):
+        current = self.head
         if self.head:
             while current.next:
                 current = current.next
                 current.next = new_element
         else:
             self.head = new_element
+    
+
+
+
+
+
+
+
+        
+
+    
         
 
 
