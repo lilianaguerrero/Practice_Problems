81767394e58736c40b1755ca6097390d3afe39b4
LilianaGuerrero <liliana.guerrerosf@gmail.com>
2020-09-12 11:48:02 -0700

    added word search

--- prefix_trie.py
+++ prefix_trie.py
@@ -1,29 +1,30 @@
 #prefix Trie with search/ add/ start with methods
 
 class Trie(object):
-   def __init__(self):
-      self.child = {} #make one dictionary called child
-   def insert(self, word):
-      current = self.child #begins at the child 
-      for l in word: 
-         if l not in current:
-            current[l] = {} #populating the tree with all unique characters
-         current = current[l] #moves down trie
-      current['#']=1 #after each word, append a # = 1
-   def search(self, word):
-      current = self.child #begins at the child 
-      for l in word: 
-         if l not in current: #starts looking through child 
-            return False
-         current = current[l] #begins at the child 
-      return '#' in current #returns True if # in current
-   def startsWith(self, prefix):
-      current = self.child
-      for l in prefix:
-         if l not in current:
-            return False
-         current = current[l]
-      return True
+    def __init__(self):
+        self.child = {} #make one dictionary called child
+    def insert(self, word):
+        current = self.child #begins at the child 
+        for l in word: 
+            if l not in current:
+                current[l] = {} #populating the tree with all unique characters
+            current = current[l] #moves down trie
+        current['#']=1 #after each word, append a # = 1 to signify end of word
+    def search(self, word):
+        current = self.child #begins at the child 
+        for l in word: 
+            if l not in current: #starts looking through child 
+                return False
+            current = current[l] #moved down a level on trie
+        return '#' in current #returns True if reached end of word and therefore a # in current
+    def startsWith(self, prefix):
+        current = self.child
+        for l in prefix: #checking for part of a word
+            if l not in current:
+                return False
+            current = current[l]
+        return True
+
 
 ob1 = Trie()
 ob1.insert("apple")
@@ -32,4 +33,5 @@ print(ob1.search("app"))
 print(ob1.startsWith("app"))
 ob1.insert("app")
 print(ob1.search("app"))
+print(ob1.printTrie)
 print(ob1.child)
